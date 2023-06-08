d91dc8536b93aa4f7c14820825a696e3a2f776d8
JeremyRojasF <jeremy.rojas.f@gmail.com>
2023-06-07 22:46:02 -0500

    Rutas con parametros

--- main.py
+++ main.py
@@ -2,32 +2,34 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+users = [
+    {
+        "id":1,
+        "nombre": "Pablo",
+        "apellido": "España",
+        "edad": 26
+    },
+    {
+        "id":2,
+        "nombre": "José",
+        "apellido": "Sanchez",
+        "edad": 23
+    }
+]
+
 @app.get("/")
 def message():
     return 'Hello World'
 
-@app.get('/login')
-def mensaje():
-    return 'Inicie sesion'
-
 
 @app.get('/users')
 def mensaje():
-    users=[
-        {
-            "nombre": "Pablo",
-            "apellido": "España"
-        }
-    ]
     return users
 
-
-@app.get('/auth/login/users')
-def mensaje():
-    users=[
-        {
-            "nombre": "Pablo",
-            "apellido": "España"
-        }
-    ]
-    return users
+@app.get('/user/{id}/edad/{edad}')
+def get_user(id, edad):
+    print(edad)
+    for user in users:
+        if user['id']== int(id) and user['edad'] == int(edad):
+            return user
+    return 'Usuario no encontrado'
